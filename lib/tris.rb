bef0281a58c00b1ff18cf4c98821436714e16a6d
Patrick David & Brandon Madsen <student@epicodus.com>
2014-07-30 16:45:31 -0700

    triangle now has a method named style that indicates the type of triangle

--- lib/tris.rb
+++ lib/tris.rb
@@ -1,5 +1,20 @@
 class Tri
   def initialize(side1, side2, side3)
-
+  @side1 = side1
+  @side2 = side2
+  @side3 = side3
   end
+
+  def style
+    if @side1 + @side2 < @side3 || @side2 + @side3 < @side1 || @side1 + @side3 < @side2
+      "not a triangle"
+    elsif @side1 == @side2 && @side2 == @side3
+      "equilateral"
+    elsif @side1 == @side2 || @side2 == @side3 || @side1 == @side3
+      "isoscoles"
+    elsif @side1 != @side2 && @side2 != @side3 && @side1 != @side3
+      "scalene"
+    end
+  end
+
 end
--- spec/tris_spec.rb
+++ spec/tris_spec.rb
@@ -6,4 +6,24 @@ describe Tri do
     test_tri = Tri.new(3, 3, 3)
     expect(test_tri).to be_an_instance_of Tri
   end
+
+  it "will determine if the triangle is an equilateral" do
+    test_tri = Tri.new(2, 2, 2)
+    expect(test_tri.style).to(eq("equilateral"))
+  end
+
+  it "will determine if the triangle is an isoscoles" do
+    test_tri = Tri.new(2, 2, 3)
+    expect(test_tri.style).to(eq("isoscoles"))
+  end
+
+  it "will determine if the triangle is an scalene" do
+    test_tri = Tri.new(2, 4, 3)
+    expect(test_tri.style).to(eq("scalene"))
+  end
+
+  it "will determine if the triangle is in fact a triangle" do
+    test_tri = Tri.new(2, 4, 9)
+    expect(test_tri.style).to(eq("not a triangle"))
+  end
 end
